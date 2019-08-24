47791ecc316c81e8de0673e53dac1e241a419bca
Mark Kuczmarski <mark@mkucz.com>
2019-08-24 11:54:55 +0900

    Use chartist.js to render c50 index chart

--- src/views/js-chart.js
+++ src/views/js-chart.js
@@ -1,158 +1,50 @@
 const m = window.preact.h
+let prevState = null
 
-function renderChart (id) {
-  const chart = document.getElementById(id)
-  if (!chart) return null
-  const ctx = chart.getContext('2d')
+function renderChart (state) {
+  if (!state.c50Summary) return
+  if (!state.c50Summary.map) return
 
-  const inst = new window.Chart(ctx, {
-    type: 'line',
-    data: [],
-    options: {
-      scales: {
-        responsive: true,
-        maintainAspectRatio: false,
-        xAxes: [
-          {
-            type: 'time',
-            time: { unit: 'month' },
-            distribution: 'series'
-          }
-        ],
-        yAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: 'Normalized C50 Index %'
-            }
-          }
-        ]
+  // memoize the state, don't re-render if nothing has changed :)
+  if (!!prevState && state.c50Summary === prevState.c50Summary) return
+  prevState = state
+
+  const beginningPrice = state.c50Summary[0]['C50Index']
+
+  const data = [] // The data for the chart
+  /**
+   * Don't show the last element because sometimes that value is wacky from the server... this should
+   * probably be fixed at some point, but for now, we'll just throw it out.
+   *  */
+
+  for (let i = 0; i < state.c50Summary.length - 1; i++) {
+    const summary = state.c50Summary[i]
+    data.push({
+      x: summary.Date,
+      y: (Number(summary['C50Index']) - beginningPrice) / beginningPrice
+    })
+  }
+
+  return new window.Chartist.Line(
+    '.ct-chart',
+    { series: [data] },
+    {
+      showLine: true,
+      axisX: {
+        showLabel: false,
+        offset: 0
+      },
+      axisY: {
+        showLabel: true,
+        offset: 0
       }
-      // tooltips: {
-      //   callbacks: {
-      //     title: function (item, data) {
-      //       var date = item.xLabel
-      //       return new moment(date)
-      //     },
-      //     label: function (item, data) {
-      //       var datasetLabel = data.datasets[item.datasetIndex].label || ''
-      //       var date = item.xLabel
-      //       var value = item.yLabel
-      //       if (datasetLabel === slug) {
-      //         return `${datasetLabel}: $${(
-      //           value * comparedMarketCap
-      //         ).toLocaleString()}`
-      //       } else {
-      //         return `${datasetLabel}: ${(
-      //           value * c50beginningPrice
-      //         ).toLocaleString()}`
-      //       }
-      //     }
-      //   }
-      // }
     }
-  })
-  return m('div', {}, inst)
+  )
 }
 
 export function JSChart (dispatch) {
-  // console.log(ChartInstance)
   return state => {
-    return m('canvas', { id: 'chart' }, renderChart('chart'))
+    renderChart(state)
+    return m('div', { class: 'ct-chart' })
   }
 }
-// function drawChart (slug) {
-//   let chartInstance = null
-//   let c50beginningPrice = null
-//   let comparedMarketCap = null
-//   let startTimeUnix = 1483196400 // 1/1/2017
-
-//   let startDate = moment()
-//     .year(2017)
-//     .startOf('year')
-
-//   let container = docum
-//   let container = document.getElementById('chartcontainer')
-//   container.innerHTML =
-//     "<canvas id='chart' style='width: 100%; height: 100%;'></canvas>"
-//   var ctx = document.getElementById('chart').getContext('2d')
-//   if (chartInstance) chartInstance.destroy()
-
-// readCSV('https://cdn.answrly.com/c50/all-coins/c50-index.csv', {
-//   headers: true
-// })
-//   .then(rows => {
-//     // Date: "2016-12-30", C50Index: "101.21617490463628", market_weighted_divisor: "17019127036.0"
-//     return rows
-//   })
-//   .then(c50IndexData => {
-//     readCSV(`https://cdn.answrly.com/c50/all-coins/${slug}.csv`, {
-//       headers: true
-//     }).then(rows => {
-//       // calculate c50 index and normalize
-//       let c50Index = c50IndexData.map(o => {
-//         let time = new moment(o['Date'])
-//         if (!c50beginningPrice) c50beginningPrice = Number(o['C50Index'])
-//         return {
-//           x: time,
-//           y: (Number(o['C50Index']) - c50beginningPrice) / c50beginningPrice
-//         }
-//       })
-
-//       rows
-//         .sort((a, b) => Number(a['time_unix']) - Number(b['time_unix']))
-//         .filter(r => {
-//           if (!comparedMarketCap && Number(r['time_unix']) >= startTimeUnix) {
-//             comparedMarketCap = Number(r['marketcap'])
-//             return true
-//           } else {
-//             return false
-//           }
-//         })[0]['close']
-
-//       let sortedComparedRows = rows.sort(
-//         (a, b) => Number(a['time_unix']) - Number(b['time_unix'])
-//       )
-
-//       let comparedData = sortedComparedRows
-//         .map(o => {
-//           let time = new moment(o['time_unix'], 'X')
-//           let normalizedPrice =
-//             (Number(o['marketcap']) - comparedMarketCap) / comparedMarketCap
-
-//           if (time.unix() >= startTimeUnix && !!normalizedPrice) {
-//             console.log(normalizedPrice)
-//             return { x: time, y: normalizedPrice }
-//           } else {
-//             return false
-//           }
-//         })
-//         .filter(x => x)
-
-//       // view-source:http://www.chartjs.org/samples/latest/scales/time/financial.html
-//       let dataStyles = {
-//         pointRadius: 0,
-//         fill: true,
-//         type: 'line',
-//         lineTension: 0,
-//         borderWidth: 2
-//       }
-
-//       draw({
-//         datasets: [
-//           {
-//             label: slug,
-//             data: comparedData,
-//             backgroundColor: 'rgba(188,188,163,.6)',
-//             ...dataStyles
-//           },
-//           {
-//             label: 'C50 Index',
-//             data: c50Index,
-//             backgroundColor: '#2875e3',
-//             ...dataStyles
-//           }
-//         ]
-//       })
-//     })
-//   })
